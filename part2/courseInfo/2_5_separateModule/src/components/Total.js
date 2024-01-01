85f7c83fe00e3a7e443861bd8c22f05231893ae1
Peter-TMK <tundepeter01@gmail.com>
2024-01-01 12:06:48 +0100

    added more exercises

--- part2/courseInfo/2_5_separateModule/src/components/Total.js
+++
@@ -1,6 +0,0 @@
-const Total = ({ parts }) => {
-  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <h3>Total exercises: {totalExercises}</h3>;
-};
-
-export default Total;
