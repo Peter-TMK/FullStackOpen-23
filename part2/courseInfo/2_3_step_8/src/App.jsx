2387c784b6c81551922f474cfa30fd2139cf297b
Peter-TMK <tundepeter01@gmail.com>
2024-01-02 06:55:40 +0100

    courseInfo complete

---
+++ part2/courseInfo/2_3_step_8/src/App.jsx
@@ -0,0 +1,63 @@
+const Course = ({ name }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <p key={part.id}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, parts) => sum + parts.exercises, 0);
+  return <h4>total of {totalExercises} exercises</h4>;
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Course name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
