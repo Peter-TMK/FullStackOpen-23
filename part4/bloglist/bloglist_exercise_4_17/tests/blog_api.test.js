91202acbad51555a23e1bc51db1fd444eb20d3a0
Peter-TMK <tundepeter01@gmail.com>
2024-02-16 12:47:49 +0100

    blogList update

--- part4/bloglist/bloglist_exercise_4_17/tests/blog_api.test.js
+++ part4/bloglist/bloglist_exercise_4_17/tests/blog_api.test.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
-const helper = require("../tests/blog_helper");
+const helper = require("./blog_helper");
 const app = require("../app");
 const api = supertest(app);
 const Blog = require("../models/blog");
