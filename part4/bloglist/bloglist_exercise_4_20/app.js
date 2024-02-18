8f6354e07f3925bf3addd0e66f7a2bf954df2e71
Peter-TMK <tundepeter01@gmail.com>
2024-02-18 17:56:35 +0100

    blogList update

---
+++ part4/bloglist/bloglist_exercise_4_20/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const config = require("./utils/config");
+require("express-async-errors");
+const app = express();
+const cors = require("cors");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+const blogRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+const tokenExtractor = require("./utils/middleware").tokenExtractor;
+
+mongoose.set("strictQuery", false);
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+// mongoose.connect(mongoUrl);
+
+app.use(cors());
+app.use(express.json());
+app.use(tokenExtractor);
+
+app.use("/api/blogs", blogRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
+
+module.exports = app;
