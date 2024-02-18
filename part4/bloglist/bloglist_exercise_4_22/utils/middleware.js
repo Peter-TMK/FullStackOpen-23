ace1d1c0efce0033ec478cf9f86f33685155bd57
Peter-TMK <tundepeter01@gmail.com>
2024-02-18 19:46:34 +0100

    blogList update

--- part4/bloglist/bloglist_exercise_4_22/utils/middleware.js
+++ part4/bloglist/bloglist_exercise_4_22/utils/middleware.js
@@ -1,5 +1,5 @@
-// const jwt = require("jsonwebtoken");
-// const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 const logger = require("./logger");
 
@@ -54,27 +54,25 @@ const tokenExtractor = (request, response, next) => {
   next();
 };
 
-// const userExtractor = async (request, response, next) => {
-//   const decodedToken = jwt.verify(request.token, process.env.SECRET);
-//   if (!decodedToken.id) {
-//     return response.status(401).json({ error: "Token missing or invalid" });
-//   }
+const userExtractor = async (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "Token missing or invalid" });
+  }
 
-//   const user = await User.findById(decodedToken.id);
-//   if (!user) {
-//     return response.status(404).json({ error: "User not found" });
-//   }
+  const user = await User.findById(decodedToken.id);
+  if (!user) {
+    return response.status(404).json({ error: "User not found" });
+  }
 
-//   request.user = user;
-//   next();
-// };
-
-// module.exports = userExtractor
+  request.user = user;
+  next();
+};
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
   tokenExtractor,
-  // userExtractor,
+  userExtractor,
 };
